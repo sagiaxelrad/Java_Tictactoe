abcd9e5d49c411a008c70ccf88b1e6066e806df3
Liam Zamir <lomsigaming@gmail.com>
2022-09-05 19:25:46 +0300

    Update 2

--- Board.java
+++ Board.java
@@ -1,8 +1,18 @@
 
+import java.util.Scanner;
+
 public class Board {
     private int[][] board;
     int k = 10;
     int c = 6;
+
+    public static void main(String[] args) {
+
+        Board board = new Board(); // create a new board
+
+        System.out.println(board);
+
+    }
 
     public Board() {
         // create a new board
@@ -15,11 +25,26 @@ public class Board {
         }
     }
 
+    public void setCoordinate(int x, int y, int value) {
+        this.board[x][y] = value;
+    }
+
     public String toString() {
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < k; i++) {
             for (int j = 0; j < c; j++) {
                 sb.append("[");
+                switch (this.board[i][j]) {
+                    case 0:
+                        sb.append(" ");
+                        break;
+                    case 1:
+                        sb.append("X");
+                        break;
+                    case 2:
+                        sb.append("O");
+                        break;
+                }
 
                 sb.append("]");
             }
--- Game.java
+++ Game.java
@@ -7,6 +7,29 @@ public class Game {
         boolean flag = true;
         Board board = new Board(); // create a new board
 
+        Player player = new Player("X"); // create a new player with mark "X"// create a new player with mark "O"
+        int counter = 0;
+        int symbol;
+
+        while (flag) {
+            System.out.println(board); // print the board
+            // set turn
+            symbol = 1;
+            if (counter % 2 != 0)
+                symbol = 2;
+
+            counter++;
+
+            // get user input
+            String row = player.getUserInput(1); // get row from user
+            String col = player.getUserInput(2); // get col from user
+
+            int rowInt = Integer.parseInt(row); // convert row to int
+            int colInt = Integer.parseInt(col); // convert col to int
+            board.setCoordinate(rowInt, colInt, symbol); // set the coordinate to the board
+        }
+
+        board.setCoordinate(1, 1, 1);
         System.out.println(board);
 
     }
