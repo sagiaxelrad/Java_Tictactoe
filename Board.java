43a6a38ac2ea7e21b4d51de1b4a67abbb50b4920
sagiaxelrad <109092539+sagiaxelrad@users.noreply.github.com>
2022-09-12 19:59:06 +0300

    Update Board.java

--- Board.java
+++ Board.java
@@ -3,14 +3,14 @@ import java.util.*;
 public class Board {
     private int[][] board;
     int k = 5;
-    int c = 5;
+    int c = 150;
 
     public Board() {
         // create a new board
 
         this.board = new int[k][c];
         for (int i = 0; i < k; i++) {
-            for (int j = 0; j < c; j++) {
+            for (int j = 0; j < c; j++){
                 this.board[i][j] = 0;
             }
         }
@@ -18,11 +18,35 @@ public class Board {
     //determine where to place symbols
     public void setCoordinate(int x, int y, int value) {
         this.board[x][y] = value;
+    
     }
-    //draw the board
+    //draw the 
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        if(c>61){
+            c = 61;
+            }
+        if(c<27){
         
+        sb.append(" ");
+        for (int l = 1; l<c; l++){
+            sb.append(" ");
+            String converted = Character.toString(l+96);
+            sb.append(converted);
+            sb.append(" ");
+        }
+        }
+        else{
+            sb.append(" ");
+            for (int l = 1; l<c; l++){
+                
+                sb.append(" ");
+                sb.append(l);
+                if(l<10){
+                sb.append(" ");}
+        }
+        }
+        sb.append("\n");
         for (int i = 1; i < k; i++) {
             for (int j = 1; j < c; j++) {
                 if (j==1){
