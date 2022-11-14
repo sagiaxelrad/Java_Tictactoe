4db94fb3ae90e27ce09399fc202eb0aacb445610
sagiaxelrad <109092539+sagiaxelrad@users.noreply.github.com>
2022-11-14 18:21:16 +0200

    lables

--- Board.java
+++ Board.java
@@ -15,17 +15,20 @@ public class Board {
     public void setCoordinate(int x, int y, int value) {
         this.board[x][y] = value;
     }
+    //test if somebody won
     public String testwin(){
         String resault = "";
+        //test collumns
         for(int l=0; l<3; l++){
             if((board[1][l]==board[2][l])&&(board[2][l]==board[3][l])&&(board[1][l]!=0)){
                     resault = "you won";
                 }}
+        //test row
         for(int m=0; m<3; m++){
             if((board[m][1]==board[m][2])&&(board[m][2]==board[m][3])&&(board[m][1]!=0)){
                     resault = "you won";
                 }}
-            
+        //test diagnal
         if((board[1][1]==board[2][2])&&(board[2][2]==board[3][3])&&(board[1][1]!=0)){
                 resault = "you won";
             }
@@ -34,9 +37,10 @@ public class Board {
         }
             return resault; 
     }
-    //draw the 
+    //draw the board
     public String toString() {
         StringBuilder sb = new StringBuilder();
+        //lable collumns
         if(c>61){
             c = 61;
             }
@@ -58,6 +62,7 @@ public class Board {
                 sb.append(" ");}
         }
         }
+        //draw the actual board
         sb.append("\n");
         for (int i = 1; i < k; i++) {
             for (int j = 1; j < c; j++) {
